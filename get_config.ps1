26502b123948386497a1ebfa223107864931c823
Lucas Ressler <lucas.ressler@t-systems.com>
2025-02-06 11:20:30 +0100

    Added log-directory config field

--- get_config.ps1
+++ get_config.ps1
@@ -3,6 +3,7 @@ function Get-Config ([String]$conf_path) {
     catch { throw Format-Error -Message "The config could not be loaded" -cause $_.Exception.Message }
     $faults = Assert-Format $config @{
         nsx_image_path = @{}
+        log_directory = @{}
         api = @{
             base_url = @{}
             catalog_ids = @{ security_groups = @{}; services = @{}; rules = @{} }
@@ -28,6 +29,7 @@ function Get-Config ([String]$conf_path) {
 
     @{
         nsx_image_path = $config.nsx_image_path
+        log_directory = $config.log_directory
         excel = $config.excel
         api = @{
             catalog_ids = $config.api.catalog_ids
