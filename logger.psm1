108155681e1b900a81f52c90564b9ea2fd66b19c
Lucas Ressler <lucas.ressler@t-systems.com>
2025-02-11 15:04:59 +0100

    Wtf, I just had the contents of api_handle.psm1 here before??

--- logger.psm1
+++ logger.psm1
@@ -1,140 +1,67 @@
-enum DeploymentStatus {
-    InProgress
-    Successful
-    Failed
+enum LogLevel {
+    Debug = 0
+    Info  = 1
+    Warn  = 2
+    Error = 3
 }
 
-class ApiHandle {
-    [Hashtable]$headers
-    [Hashtable]$tenant_map
-    [String]$refresh_token
+class Log {
+    [DateTime]$date
+    [LogLevel]$level
+    [String]$section
+    [String]$message
 
-    [String]$username
-    [String]$password
-
-    [String]$url_refresh_token
-    [String]$url_deployments
-    [String]$url_project_id
-    [String]$url_items
-    [String]$url_login
-    
-    ApiHandle ([Hashtable]$config) {
-        $this.username = $config.api.credentials.username
-        $this.password = $config.api.credentials.password
-        $this.url_refresh_token = $config.api.urls.refresh_token
-        $this.url_deployments = $config.api.urls.deployments
-        $this.url_project_id = $config.api.urls.project_id
-        $this.url_items = $config.api.urls.items
-        $this.url_login = $config.api.urls.login
-        $this.tenant_map = @{}
+    Log ([LogLevel]$level, [String]$section, [String]$message) {
+        $this.date = Get-Date
+        $this.level = $level
+        $this.section = $section
+        $this.message = $message.Trim()
     }
+}
 
-    [Void]Init () {
-        # get refresh token
-        try {
-            $body = @{
-                username = $this.username
-                password = $this.password
-            } | ConvertTo-Json
-            $response = Invoke-RestMethod $this.url_refresh_token -Method Post -ContentType "application/json" -Body $body -TimeoutSec 5
-            $this.refresh_token = $response.refresh_token
-        }
-        catch {
-            throw Format-Error -Message "Failed to obtain refresh token!" -Cause $_.Exception.Message -Hints @(
-                "Ensure that you're connected to the Admin-LAN"
-                "Ensure your username and password are valid"
-            )
-        }
+class Logger {
+    [Log[]]$logs = @()
+    [String]$section = $null
+    [LogLevel]$min_write_level = [LogLevel]::Info
+    [System.Management.Automation.Host.PSHostUserInterface]$UI
 
-        # get access token
-        try {
-            $body = @{
-                refreshToken = $this.refresh_token
-            } | ConvertTo-Json
-            $response = Invoke-RestMethod $this.url_login -Method Post -ContentType "application/json" -Body $body
-            $access_token = $response.token
+    Logger () { $this.UI = $null }
+    Logger ([System.Management.Automation.Host.PSHostUserInterface]$UI) { $this.UI = $UI }
 
-            $this.headers = @{
-                Authorization = "Bearer $access_token"
-            }
-        }
-        catch {
-            throw Format-Error -Message "Failed to obtain access token!" -Cause $_.Exception.Message -Hints @(
-                "Ensure your connection is stable"
-            )
+    [Void] Log ([LogLevel]$level, [String]$message) {
+        $this.logs += [Log]::new($level, $this.section, $message)
+        if ($null -eq $this.UI) { return }
+        if ($level -lt $this.min_write_level) { return }
+        switch ($level) {
+            ([LogLevel]::Debug) { $this.UI.WriteDebugLine($message) }
+            ([LogLevel]::Info) { $this.UI.WriteLine($message) }
+            ([LogLevel]::Warn) { $this.UI.WriteWarningLine($message) }
+            ([LogLevel]::Error) { $this.UI.WriteErrorLine($message) }
         }
     }
 
-    [String]TenantID ([String]$tenant) {
-        [String]$failed = "-1"
-        [String]$cached = $this.tenant_map[$tenant]
-        if ($cached -eq $failed) { throw "Tenant $tenant cannot be accessed" }
-        elseif ($cached) { return $this.tenant_map[$tenant] }
+    [Void] Debug ([String]$message) { $this.Log([LogLevel]::Debug, $message) }
+    [Void] Info ([String]$message) { $this.Log([LogLevel]::Info, $message) }
+    [Void] Warn ([String]$message) { $this.Log([LogLevel]::Warn, $message) }
+    [Void] Error ([String]$message) { $this.Log([LogLevel]::Error, $message) }
 
-        # get project id
-        try {
-            $url = "$($this.url_project_id)?`$filter=name eq '$tenant'" 
-            $response = Invoke-RestMethod $url -Method Get -Headers $this.headers
-        }
-        catch {
-            $this.tenant_map[$tenant] = $failed
-            throw Format-Error -Message "Failed to get project id for tenant '$tenant'!" -Cause $_.Exception.Message
-        }
-
-        if ($response.content.Length -eq 1) {
-            [String]$id = $response.content[0].id
-            $this.tenant_map[$tenant] = $id
-            return $id
-        }
-        else {
-            $this.tenant_map[$tenant] = $failed
-            throw Format-Error -Message "Failed to get project id!" -Hints @(
-                "Expected exactly 1 project with the given Tenant name, found $($response.content.Length)"
-                "Maybe '$tenant' is not a valid tenant name?"
-            )
+    [String[]] GetLogs () {
+        return $this.logs | ForEach-Object {
+            [Log]$log = $_
+            [String]$date_str = $log.date.ToString("yyyy-MM-dd HH:mm:ss.ff")
+            [String]$level_str = "$($log.level)".ToUpper(); $level_str += " " * (5 - $level_str.Length)
+            [String]$section_str = if ($log.section) { " [ $($log.section) ] " }
+            @($log.message.Split([Environment]::NewLine) | ForEach-Object {
+                [String]$line = "$date_str  $level_str $section_str $_"
+                $date_str = "    ^           ^     "    
+                $level_str = "  ^  "
+                $section_str = if ($section_str) { " " * [Math]::Floor($section_str.Length / 2) + "^" + " " * [Math]::Ceiling($section_str.Length / 2 - 1) }
+                $line
+            }) -join "`n"
         }
     }
 
-    [Object] Get ([String]$url) {
-        return Invoke-RestMethod $url -Method Get -Headers $this.headers
-    }
-    [Object] Post ([String]$url, [Hashtable]$body) {
-        return Invoke-RestMethod $url -Method Post -ContentType "application/json" -Headers $this.headers -Body ($body | ConvertTo-Json)
-    }
-
-    [String] Deploy ([String]$name, [String]$tenant, [String]$catalog_id, [Hashtable]$inputs) {
-        $body = @{
-            projectId = $this.TenantID($tenant)
-            deploymentName = $name
-            inputs = $inputs
-        }
-
-        $response = $this.Post("$($this.url_items)/$catalog_id/request", $body)
-        $deployment_id = $response.deploymentId
-        if ($null -eq $deployment_id) { throw "Received invalid response: $($response | ConvertTo-Json)" }
-        return $deployment_id
-    }
-
-    [DeploymentStatus] CheckDeployment ([String]$deployment_id) {
-        $response = $this.Get("$($this.url_deployments)/$deployment_id")
-        switch ($response.status) {
-            "CREATE_INPROGRESS" { return [DeploymentStatus]::InProgress }
-            "CREATE_SUCCESSFUL" { return [DeploymentStatus]::Successful }
-            "CREATE_FAILED" { return [DeploymentStatus]::Failed }
-        }
-        throw "Received invalid response: $($response | ConvertTo-Json)"
-    }
-
-    [DeploymentStatus] WaitForDeployment ([String]$deployment_id) {
-        $status = $null
-        $complete = $false
-        $wait_time = 0
-        while (-not $complete) {
-            Start-Sleep $wait_time
-            $status = $this.CheckDeployment($deployment_id)
-            $complete = $status -ne [DeploymentStatus]::InProgress
-            $wait_time++
-        }
-        return $status
+    [Void] Save ([String]$path) {
+        $this.GetLogs() -join "`n" | Set-Content -Path $path
     }
 }
