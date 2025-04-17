6ed2f4110cb6d8a562c74d7860f46e783633aae3
Lucas Ressler <lucas.ressler@t-systems.com>
2025-04-17 09:36:34 +0000

    Added nsx_api_handle

---
+++ nsx_api_handle.psm1
@@ -0,0 +1,49 @@
+using module ".\shared_types.psm1"
+
+class NsxApiHandle {
+    [String]$base_url
+    [Hashtable]$headers
+
+    NsxApiHandle ([String]$base_url) {
+        if (-not $base_url)         { throw "NSX Host Domain was not provided" }
+        if (-not $env:nsx_user)     { throw "NSX Username was not provided" }
+        if (-not $env:nsx_password) { throw "NSX Password was not provided" }
+        [Byte[]]$bytes = [System.Text.Encoding]::UTF8.GetBytes("${env:nsx_user}:${env:nsx_password}")
+        [String]$encoded = [Convert]::ToBase64String($bytes) 
+        $this.base_url = $base_url
+        $this.headers = @{
+            Authorization = "Basic $encoded"
+        }
+    }
+
+    [PSCustomObject] ApiGet ([String]$path) {
+        [String]$url = "$($this.base_url)/api/v1/$path"
+        return Invoke-RestMethod -Method Get -Uri $url -Headers $this.headers
+    }
+    [PSCustomObject] GetSecurityGroup ([String]$tenant, [String]$name) {
+        return $this.ApiGet("infra/domains/default/groups/${tenant}_grp-ips-${name}")
+    }
+    [PSCustomObject] GetService ([String]$tenant, [String]$name) {
+        return $this.ApiGet("infra/services/${tenant}_svc-${name}")
+    }
+    [PSCustomObject] GetRule ([String]$tenant, [String]$gateway, [String]$name) {
+        [String]$policy = "${tenant}_Customer_Perimeter_${gateway}_Section01"
+        return $this.ApiGet("infra/domains/default/security-policies/${policy}/rules/${tenant}_pfwpay-${name}_dfw")
+    }
+    [Boolean] ResourceExists ([DataPacket]$data_packet) {
+        [String]$tenant = $data_packet.tenant
+        [String]$name = $data_packet.GetApiConversion([ApiAction]::Create).name
+        [String]$gateway = $data_packet.data.gateway -replace '^\S+\s*', ""
+        try {
+            switch ($data_packet.resource_config.id) {
+                ([ResourceId]::SecurityGroup) { return $null -ne $this.GetSecurityGroup($tenant, $name)  }
+                ([ResourceId]::Service)       { return $null -ne $this.GetService($tenant, $name)        }
+                ([ResourceId]::Rule)          { return $null -ne $this.GetRule($tenant, $gateway, $name) }
+            }
+        } catch [System.Net.WebException] {
+            if ($_.Exception.Response -and $_.Exception.Response.StatusCode -eq 404) { return $false }
+            else { throw $_.Exception }
+        }
+        return $false
+    }
+}
