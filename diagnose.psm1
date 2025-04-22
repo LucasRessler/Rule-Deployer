b54f29c42d218cac4416831a62e5974030dabe58
Lucas Ressler <lucas.ressler@t-systems.com>
2025-04-22 13:07:40 +0200

    Update diagnose.psm1 - Step 1

--- diagnose.psm1
+++ diagnose.psm1
@@ -1,70 +1,127 @@
+using module ".\nsx_api_handle.psm1"
 using module ".\shared_types.psm1"
 using module ".\io_handle.psm1"
+
+function CheckDependenciesFromImg {
+    param ([IOHandle]$io_handle, [DataPacket]$failed_packet)
+    if ($failed_packet.resource_config.id -ne [ResourceID]::Rule) { return @() }
+    [String[]]$missing_depends = @()
+    [String]$tenant = $failed_packet.tenant
+    foreach ($used_service in $failed_packet.data["services"]) {
+        [String[]]$service_keys = @($tenant, "services", $used_service)
+        if ($null -eq $io_handle.GetImage($service_keys)) { $missing_depends += "$used_service (Service)" }
+    }
+    foreach ($used_source in $failed_packet.data["sources"]) {
+        [String[]]$source_keys = @($tenant, "security_groups", $used_source)
+        if ($null -eq $io_handle.GetImage($source_keys)) { $missing_depends += "$used_source (Source Security Group)" }
+    }
+    foreach ($used_destination in $failed_packet.data["destinations"]) {
+        [String[]]$desitnation_keys = @($tenant, "security_groups", $used_destination)
+        if ($null -eq $io_handle.GetImage($desitnation_keys)) { $missing_depends += "$used_destination (Destination Security Group)" }
+    }
+    return $missing_depends
+}
+
+function CheckDependeesFromImg {
+    param ([IOHandle]$io_handle, [DataPacket]$failed_packet, [Bool]$tried_delete)
+    if (-not $tried_delete -or $failed_packet.resource_config.id -eq [ResourceId]::Rule) { return @() }
+    [String[]]$dependees = @()
+    [String]$name = $failed_packet.data["name"]
+    [Hashtable]$relevant_rules = @{}
+    [Hashtable]$this_tenant = $io_handle.nsx_image[$failed_packet.tenant]
+    if ($this_tenant -and $this_tenant["rules"]) { $relevant_rules = $this_tenant["rules"] }
+    foreach ($gateway in $relevant_rules.Keys) {
+        [Hashtable]$service_requests = $relevant_rules[$gateway]
+        foreach ($service_request in $service_requests.Keys) {
+            [Hashtable]$indeces = $service_requests[$service_request]
+            foreach ($index in $indeces.Keys) {
+                [Hashtable]$rule = $indeces[$index]
+                [String[]]$depends = @(
+                    $rule["services"]
+                    $rule["sources"]
+                    $rule["destinations"]
+                )
+                if ($name -in $depends) { $dependees += $rule["name"] }
+            }
+        }
+    }
+    return $dependees
+}
+
+function CheckDependenciesFromApi {
+    param ([NsxApiHandle]$nsx_api_handle, [DataPacket]$failed_packet)
+    if ($failed_packet.resource_config.id -ne [ResourceId]::Rule) { return @() }
+    [String[]]$missing_depends = @()
+    [String]$tenant = $failed_packet.tenant
+    foreach ($used_service in $failed_packet.data["services"]) {
+        [DataPacket]$service_dp = [DataPacket]::New(@{ name = $used_service }, @{ id = [ResourceId]::Service }, $tenant, $null)
+        if (-not $nsx_api_handle.ResourceExists($service_dp)) { $missing_depends += "$used_service (Service)" }
+    }
+    foreach ($used_source in $failed_packet.data["sources"]) {
+        [DataPacket]$source_dp = [DataPacket]::New(@{ name = $used_source }, @{ id = [ResourceId]::SecurityGroup }, $tenant, $null)
+        if (-not $nsx_api_handle.ResourceExists($source_dp)) { $missing_depends += "$used_source (Source Security Group)" }
+    }
+    foreach ($used_destination in $failed_packet.data["destinations"]) {
+        [DataPacket]$destination_dp = [DataPacket]::New(@{ name = $used_destination }, @{ id = [ResourceId]::SecurityGroup }, $tenant, $null)
+        if (-not $nsx_api_handle.ResourceExists($destination_dp)) { $missing_depends += "$used_destination (Destination Security Group)" }
+    }
+    return $missing_depends
+}
+
+function CheckDependeesFromApi {
+    param ([NsxApiHandle]$nsx_api_handle, [DataPacket]$failed_packet, [Bool]$tried_delete)
+    if (-not $tried_delete -or $failed_packet.resource_config.id -eq [ResourceId]::Rule) { return @() }
+    [String]$dependency_path = "/" + $nsx_api_handle.ResourcePath($failed_packet)
+    [String]$payload_rules_path = $nsx_api_handle.RulePath($failed_packet.tenant, "Payload", "")   -replace '[^/]+$', ""
+    [String]$internet_rules_path = $nsx_api_handle.RulePath($failed_packet.tenant, "Internet", "") -replace '[^/]+$', ""
+    return @(
+        $nsx_api_handle.ApiGet($payload_rules_path).results
+        $nsx_api_handle.ApiGet($internet_rules_path).results
+    ) | Where-Object {
+        $dependency_path -in $_.services -or `
+        $dependency_path -in $_.source_groups -or `
+        $dependency_path -in $_.destination_groups
+    } | ForEach-Object { $_.id }
+}
 
 function DiagnoseFailure {
     param (
         [IOHandle]$io_handle,
         [DataPacket]$failed_packet,
-        [ApiAction[]]$failed_actions
+        [ApiAction[]]$failed_actions,
+        [NsxApiHandle]$nsx_api_handle
     )
 
     [Bool]$tried_create = [ApiAction]::Create -in $failed_actions
     [Bool]$tried_update = [ApiAction]::Update -in $failed_actions
     [Bool]$tried_delete = [ApiAction]::Delete -in $failed_actions
-    [String]$tenant = $failed_packet.tenant
-    [ResourceId]$resource_id = $failed_packet.resource_config.id
-    [Bool]$tracked = $null -ne $io_handle.GetImage($failed_packet.GetImageKeys())
 
-    # Check dependencies
-    [String[]]$depends_not_found = @()
-    if ($resource_id -eq [ResourceId]::Rule) {
-        foreach ($used_service in $failed_packet.data["services"]) {
-            [String[]]$service_keys = @($tenant, "services", $used_service)
-            if ($null -eq $io_handle.GetImage($service_keys)) { $depends_not_found += "$used_service (Service)" }
-        }
-        foreach ($used_source in $failed_packet.data["sources"]) {
-            [String[]]$source_keys = @($tenant, "security_groups", $used_source)
-            if ($null -eq $io_handle.GetImage($source_keys)) { $depends_not_found += "$used_source (Source Security Group)" }
-        }
-        foreach ($used_destination in $failed_packet.data["destinations"]) {
-            [String[]]$desitnation_keys = @($tenant, "security_groups", $used_destination)
-            if ($null -eq $io_handle.GetImage($desitnation_keys)) { $depends_not_found += "$used_destination (Destination Security Group)" }
-        }
+    [Bool]$tracked = if ($nsx_api_handle) {
+        $nsx_api_handle.ResourceExists($failed_packet)
+    } else {
+        $null -ne $io_handle.GetImage($failed_packet.GetImageKeys())
     }
 
+    [String[]]$missing_depends = if ($nsx_api_handle) {
+        CheckDependenciesFromApi $nsx_api_handle $failed_packet
+    } else {
+        CheckDependenciesFromImg $io_handle $failed_packet
+    }
 
-    # Check reverse dependencies
-    [String[]]$dependees_found = @()
-    if ($tried_delete -and $resource_id -ne [ResourceId]::Rule) {
-        [String]$name = $failed_packet.data["name"]
-        [Hashtable]$relevant_rules = @{}
-        [Hashtable]$this_tenant = $io_handle.nsx_image[$tenant]
-        if ($this_tenant -and $this_tenant["rules"]) { $relevant_rules = $this_tenant["rules"] }
-        foreach ($gateway in $relevant_rules.Keys) {
-            [Hashtable]$service_requests = $relevant_rules[$gateway]
-            foreach ($service_request in $service_requests.Keys) {
-                [Hashtable]$indeces = $service_requests[$service_request]
-                foreach ($index in $indeces.Keys) {
-                    [Hashtable]$rule = $indeces[$index]
-                    [String[]]$depends = @(
-                        $rule["services"]
-                        $rule["sources"]
-                        $rule["destinations"]
-                    )
-                    if ($name -in $depends) { $dependees_found += $rule["name"] }
-                }
-            }
-        }
+    [String[]]$dependees_found = if ($nsx_api_handle) {
+        CheckDependeesFromApi $nsx_api_handle $failed_packet $tried_delete
+    } else {
+        CheckDependeesFromImg $io_handle $failed_packet $tried_delete
     }
     
     # Give feedback
     switch ($true) {
         ($tried_create -and $tried_update) {
-            if ($depends_not_found.Count) {
+            if ($missing_depends.Count) {
             return @( # Can only happen for FW Rules
                 "Make sure that all security groups and services used in the rule exist"
                 "It's likely that one or more of the following resources don't exist:"
-                @($depends_not_found | ForEach-Object { "- '$_'" })
+                @($missing_depends | ForEach-Object { "- '$_'" })
                 "Note: I can only make statements for resources that were modified with this tool"
             ) } else {
             return @(
@@ -73,11 +130,11 @@ function DiagnoseFailure {
             ) }
         }
         ($tried_create) {
-            if ($depends_not_found.Count) {
+            if ($missing_depends.Count) {
             return @( # Can only happen for FW Rules
                 "Make sure that all security groups and services used in the rule exist"
                 "It's likely that one or more of the following resources don't exist:"
-                @($depends_not_found | ForEach-Object { "- '$_'" })
+                @($missing_depends | ForEach-Object { "- '$_'" })
                 "Note: I can only make statements for resources that were modified with this tool"
             ) } elseif ($tracked) {
             return @(
@@ -98,11 +155,11 @@ function DiagnoseFailure {
                 "The resource was not found in the NSX-Image"
                 "It's likely that it doesn't exist yet"
                 "You could try creating it instead"
-            } elseif ($depends_not_found.Count) {
+            } elseif ($missing_depends.Count) {
             return @( # Can only happen for FW Rules
                 "Make sure that all security groups and services used in the rule exist"
                 "It's likely that one or more of the following resources don't exist:"
-                @($depends_not_found | ForEach-Object { "- '$_'" })
+                @($missing_depends | ForEach-Object { "- '$_'" })
                 "Note: I can only make statements for resources that were modified with this tool"
             ) } else {
             return @(
